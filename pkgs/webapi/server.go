0d45bee4d13e64ebd8f16ab77be48ed610e7c35a
zeko <41507889+ZekoTheFox@users.noreply.github.com>
2024-03-25 23:44:46 -0700

    embed overlay into web server

--- pkgs/webapi/server.go
+++ pkgs/webapi/server.go
@@ -1,7 +1,9 @@
 package webapi
 
 import (
+	_ "embed"
 	"fmt"
+	"io"
 	"log"
 	http "net/http"
 )
@@ -11,11 +13,18 @@ type Image struct {
 }
 
 var (
+	//go:embed overlay/index.html
+	overlayFile string
+
 	Images chan Image = make(chan Image)
 )
 
 func StartWebAPI(port int) {
 	http.HandleFunc("GET /image", handleImage)
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=UTF-8")
+		io.WriteString(w, overlayFile)
+	})
 
 	address := "127.0.0.1:" + fmt.Sprint(port)
 
